d2e97b8703e8df917ac4d703188b2970a3460257
DuncanWillis <60787763+DuncanWillis@users.noreply.github.com>
2020-10-24 13:11:32 +0100

    Add files via upload

---
+++ Testfortxt/Testfortxt/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Testfortxt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"\Users\Duncan\Desktop\Mac Visual Studio C#\Projects\Personal Practise\Testfortxt\textFile.txt");
+            Console.WriteLine("The textfile contains the following text: {0}", text);
+            Console.ReadKey();
+
+            //it seems to be that for the reading of a file to work on a Mac, you have to explicitly write the location using normal spaces
+        }
+    }
+}
